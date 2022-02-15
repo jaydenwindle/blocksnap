b0d9b8df1e15dad3d74fd616c8620111c480ae8c
lancekrogers <lancekrogers@gmail.com>
2022-02-15 16:32:11 -0700

    updated models, removed profile

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,27 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Snapshot(models.Model):
     contract_address = models.CharField(max_length=42)
+    user_address = models.CharField(max_length=42, primary_key=True)
     start_blocknumber = models.IntegerField(default=3914495)
     last_snapshot_block = models.IntegerField()
     public = models.BooleanField(default=True)
     filters = models.JSONField()
     url = models.URLField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField()
 
     def __str__(self):
         return f"{self.contract_address} Snapshot: {self.pk}"
 
-
-class Profile(models.Model):
-    user = models.OneToOneField(User)
-    address = models.CharField(max_length=42, primary_key=True)
-    snapshots = models.ManyToManyField(Snapshot)
-
-    def __str__(self):
-        return f"{self.address}"
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        self.modified = timezone.now()
+        return super(User, self).save(*args, **kwargs)
 
 
 class Contract(models.Model):
