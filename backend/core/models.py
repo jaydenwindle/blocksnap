b9f3e5a40257843251df0b219a8b84175d3fea3c
lancekrogers <lancekrogers@gmail.com>
2022-02-15 17:32:34 -0700

    added docstring to models

--- backend/core/models.py
+++ backend/core/models.py
@@ -3,12 +3,19 @@ from django.utils import timezone
 
 
 class Snapshot(models.Model):
+    """
+    contract_address: Token Contract Address
+    user_address: Address of the user requesting a snapshot
+    start_blocknumber: block number to start indexing on
+    last_snapshot_block: last block in snapshot
+    public: Should the snapshot be publicly accessible or private
+    url: URL where the snapshot is stored
+    """
     contract_address = models.CharField(max_length=42)
     user_address = models.CharField(max_length=42, primary_key=True)
     start_blocknumber = models.IntegerField(default=3914495)
     last_snapshot_block = models.IntegerField()
     public = models.BooleanField(default=True)
-    filters = models.JSONField()
     url = models.URLField()
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField()
@@ -19,13 +26,30 @@ class Snapshot(models.Model):
     def save(self, *args, **kwargs):
         ''' On save, update timestamps '''
         self.modified = timezone.now()
-        return super(User, self).save(*args, **kwargs)
+        return super(Snapshot, self).save(*args, **kwargs)
 
 
 class Contract(models.Model):
+    """
+    address: wallet address
+    deployed_block_number: block number contract was created at
+    abi: contract ABI
+    """
     address = models.CharField(max_length=42)
     deployed_block_number = models.IntegerField(default=3914495)
     abi = models.JSONField()
 
     def __str__(self):
         return f"{self.address}"
+
+
+class Filter(models.Model):
+    """
+    filters : JSON object of filteres to be applied to a snapshot
+    filter_hash: keccak hash of filter json
+    url: url of stored filtered snapshot
+    """
+    snapshot = models.ForiegnKey(Snapshot, on_delete=models.CASCADE)
+    filters = models.JSONField()
+    filter_hash = models.CharField(max_length=64)
+    url = models.URLField()
