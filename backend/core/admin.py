8af4547eaebc80f632d26fdcccc17f81e8a1f97f
lancekrogers <lancekrogers@gmail.com>
2022-02-18 15:54:42 -0700

    updated admin and readme

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from core.models import Filter, Contract, Snapshot
 # Register your models here.
+
+
+admin.site.register(Snapshot)
+admin.site.register(Filter)
+admin.site.register(Contract)
